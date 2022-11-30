80f49fcb8ed0b409be720aa703335b544d836956
igoril82 <ilanger1982@gmail.com>
2022-11-30 19:52:11 +0200

    First commmit

---
+++ Docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"IgorPhone","l":"Contacts"},{"p":"IgorPhone","l":"Main"},{"p":"IgorPhone","l":"People"},{"p":"IgorPhone","l":"PhoneBook"}];updateSearchResults();
---
+++ Main.java
@@ -0,0 +1,21 @@
+package IgorPhone;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        PhoneBook m = new PhoneBook();
+        m.userMenu();
+
+
+    }
+
+
+}
+
+
+
+
+
